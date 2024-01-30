f82d3a21d741368aea3b6057219dcc61b1d9130d
Harsh Rana <156346015+harshrana84@users.noreply.github.com>
2024-01-30 11:54:13 +0000

    add node js

--- api/server.js
+++ api/server.js
@@ -1,18 +1,20 @@
-// server.js
-
 const path = require('path');
 const fs = require('fs');
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
-const directoryPath = path.join(__dirname, 'imaged');
+app.use(cors({ origin: '*' })); // Allow requests from any origin. Adjust as needed.
+
+const directoryPath = path.join(__dirname, '../imaged');
 
 app.get('/getFolders', function (req, res) {
   fs.readdir(directoryPath, function (err, files) {
     if (err) {
       return res.status(500).send('Unable to scan directory: ' + err);
     }
-    res.send(files);
+    res.json(files);
   });
 });
 
